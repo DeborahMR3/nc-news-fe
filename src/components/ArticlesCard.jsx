7cca43d876abe754690939ed6ff6800ed1511701
deborah reis <deborahmreis3@gmail.com>
2025-06-23 20:46:45 +0100

    Updated date display

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,6 +1,7 @@
 function ArticlesCard(props) {
   const { title, author, topic, created_at, votes, comment_count, article_img_url } = props.article
 
+  const publishedDate = new Date(created_at);
 return (
 <section>
   <h1>Title: {title}</h1>
@@ -8,11 +9,10 @@ return (
   <p>Topic: {topic}</p>
   <p>Votes: {votes}</p>
   <p>Comments: {comment_count}</p>
-  <p>Date published: {created_at}</p>
+  <p>Date published: {publishedDate.toLocaleDateString()}</p>
   <img src={article_img_url} alt={`Image for ${title} Article`}/>
 </section>
 )
-
 
 }
 export default ArticlesCard;
