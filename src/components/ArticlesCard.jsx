548dc0b3c7ed3045377325ba08220a62ec1a6b7b
deborah reis <deborahmreis3@gmail.com>
2025-06-23 19:47:38 +0100

    Add ArticlesCard.jsx

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,3 +1,18 @@
-function ArticleCard() {
+function ArticlesCard(props) {
+  const { title, author, topic, created_at, votes, comment_count, article_img_url } = props.article
+
+return (
+<section>
+  <h1>Title: {title}</h1>
+  <p>Author: {author}</p>
+  <p>Topic: {topic}</p>
+  <p>Votes: {votes}</p>
+  <p>Comments: {comment_count}</p>
+  <p>Date pusblished: {created_at}</p>
+  <img src={article_img_url} alt="Article Image" />
+</section>
+)
+
 
 }
+export default ArticlesCard;
