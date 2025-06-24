2bbdef09537d4616503ab89198c805c47ca7d19a
deborah reis <deborahmreis3@gmail.com>
2025-06-24 16:32:37 +0100

    Created link from ArticleCards to ArticlesPage

--- src/components/ArticlesCard.jsx
+++ src/components/ArticlesCard.jsx
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 function ArticlesCard(props) {
-  const { title, author, topic, created_at, votes, comment_count, article_img_url } = props.article
+  const { title, author, topic, created_at, votes, comment_count, article_img_url, article_id  } = props.article
+
   const publishedDate = new Date(created_at);
 
    console.log(props.article)
 
   return (
   <section className="article-card">
+  <Link to={`/articles/${article_id}`}>
     <h2>{title}</h2>
     <p>Author: {author}</p>
     <p>Topic: {topic}</p>
@@ -13,7 +17,8 @@ function ArticlesCard(props) {
     {/* <p>Comments: {comment_count}</p> */}
     <p>Date published: {publishedDate.toLocaleDateString()}</p>
     <img src={article_img_url} alt={`Image for ${title} Article`}/>
+  </Link>
   </section>
-  )
+  );
 }
 export default ArticlesCard;
