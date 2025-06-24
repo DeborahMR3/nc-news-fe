23983e56b5f2cf732fcb8804916ab60af7ed7b09
deborah reis <deborahmreis3@gmail.com>
2025-06-24 12:00:14 +0100

    working on ArticlePage.jsx

--- src/components/ArticlePage.jsx
+++ src/components/ArticlePage.jsx
@@ -1,1 +1,29 @@
 import { useParams } from "react-router-dom";
+
+import { useState, useEffect } from "react";
+
+function ArticlePage() {
+  const { article_id } = useParams();
+
+  const [article, setArticle] = useState(null);      // guarda o artigo recebido article_id
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  console.log("id deste artigo é:", article_id);
+
+ useEffect(() => {
+  setIsLoading(true);
+  fetch(`https://deborah-nc-news.onrender.com/api/articles/${article_id}`)
+  .then((res) => {
+    if (!res.ok) throw new Error("Artigo não encontrado");
+    return res.json();
+  })
+  .then((dataFromApi) => {
+    setArticle(dataFromApi);
+    setError(false)
+  })
+  .catch(() => setError("Erro ao carregar artigo"))
+  .finally(() => setIsLoading(false));
+
+ }, [article_id]);  // executa toda vez que esse valor mudar
+}
