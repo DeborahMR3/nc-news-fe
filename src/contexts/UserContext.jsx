8ffc58d569fdb8e0edb492fc45e673eaf5bfe5ee
deborah reis <deborahmreis3@gmail.com>
2025-06-25 15:59:23 +0100

    Create userContext.jsx

--- src/contexts/UserContext.jsx
+++ src/contexts/UserContext.jsx
@@ -1,10 +1,14 @@
-import { createContext } from 'react';
+import { createContext, useState } from "react";
 
+export const UserContext = createContext();
+
+// criar o Provider, que vai levar esse dado pra toda a app
 export const UserProvider = ({ children }) => {
-  const loggedInUser = { name: 'obi', id: 1 }; // valor inicial, fixo
+  // guardar o usuário logado (por enquanto, fixo
+  const [user, setUser] = useState({ username: "jessjelly" });
 
   return (
-    <UserContext.Provider value={{ loggedInUser }}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   );
