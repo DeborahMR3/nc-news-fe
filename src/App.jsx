81bc00c193c793deea01b4efdad0f9e8305db01c
deborah reis <deborahmreis3@gmail.com>
2025-06-27 12:14:49 +0100

    Add route to specif topin in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { UserProvider } from './contexts/UserContext';
 import ArticlesList from "./components/ArticlesList";
 import ArticlePage from "./components/ArticlePage";
 import TopicsList from './components/TopicsList';
+import TopicsPage from "./components/TopicsPage";
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<ArticlesList />} />
         <Route path="/articles/:article_id" element={<ArticlePage />} />
         <Route path="/topics" element={<TopicsList />} />
+        <Route path="/topics/:specific_topic" element={<TopicsPage />} />
     </Routes>
     </div>
     </UserProvider>
